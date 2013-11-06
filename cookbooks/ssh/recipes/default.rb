044834afcd88189a9ab820991d44c978dd7c6939
Luke Winikates and Wai Lun Mang <pair+lwinitkates+mang@pivotallabs.com>
2013-11-06 10:47:31 -0800

    add chef script to create public key and add it to authorized_keys and known_hosts

--- cookbooks/ssh/recipes/default.rb
+++ cookbooks/ssh/recipes/default.rb
@@ -3,3 +3,12 @@ execute "create /root/.ssh directory" do
   creates '/root/.ssh'
   command 'mkdir -p /root/.ssh'
 end
+
+execute "generate ssh key" do
+  user 'root'
+  creates '/root/.ssh/id_rsa.pub'
+  command 'ssh-keygen -t rsa -q -f /root/.ssh/id_rsa -P "" &&
+           cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys &&
+           ssh-keyscan -t rsa localhost.localdomain >> /root/.ssh/known_hosts &&
+           ssh-keyscan -t rsa localhost >> /root/.ssh/known_hosts'
+end
