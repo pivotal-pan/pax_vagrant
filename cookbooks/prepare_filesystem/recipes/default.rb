ca1882395cf0a6f895e05e11a5edc120635a45da
Luke Winikates and Suraj Waghulde <pair+lwinitkates+swaghulde@pivotallabs.com>
2013-11-06 16:55:58 -0800

    create zookeeper's data directory and formate the hdfs namenode

--- cookbooks/prepare_filesystem/recipes/default.rb
+++ cookbooks/prepare_filesystem/recipes/default.rb
@@ -1,4 +1,4 @@
-fancy_directories = ["/opt/apache/hadoop-0.20.2-cdh3u5/logs/", "/var/cetasdata"]
+fancy_directories = ["/opt/apache/hadoop-0.20.2-cdh3u5/logs/", "/var/cetasdata", "/var/cetasdata/zookeeper"]
 
 fancy_directories.each do |dir|
   directory dir do
@@ -7,4 +7,10 @@ fancy_directories.each do |dir|
     mode 0777
     action :create
   end
+end
+
+execute "format hdfs" do
+  user 'root'
+  creates '/var/cetasdata/hdfs/name'
+  command 'export JAVA_HOME=/opt/oracle/java && /opt/apache/hadoop/bin/hadoop namenode -format'
 end
